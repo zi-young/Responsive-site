08eecfb2279385aae6e4ae379df8650957274551
zi-young <jypark912@naver.com>
2023-07-05 22:21:12 +0900

    pc수정

--- js/script.js
+++ js/script.js
@@ -96,21 +96,9 @@ prev.click(function(){
 })
 //prev 3-4-1-2 순으로 정렬
 
-
-// mobile-gallery
-// const galleryWrap = $('.gallery-wrap');
-// let offset=galleryWrap.offset().left;
-// // console.log(offset)
-
-// galleryWrap.on({mousemove(e){
-//     // console.log(e.pageX)
-//     if(e.pageX >=  1560 ){
-//         return false;
-//     }else{
-//         $(this).css({left: -e.pageX})   
-//     }
-// }})
-
-
-
-
+// $('.type').typed({
+//   strings:['빈 공간에 가치를 담다'],
+//   typeSpeed:150,
+//   backDelay:200,
+//   loop: true
+// });
