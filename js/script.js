477dd75cde57598c0c02426b9d46d6b6b370f0df
jiyoung <jypark912@naver.com>
2023-07-13 18:12:47 +0900

    반응형완료

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 0,   // 슬라이드 사이 여백
 
-    slidesPerView : 'auto', // 한 슬라이드에 보여줄 갯수
+/*     slidesPerView : 'auto',  */// 한 슬라이드에 보여줄 갯수
     centeredSlides: true,    //센터모드
     autoplay: {     //자동슬라이드 (false-비활성화)
-      delay: 200000, // 시간 설정
+      delay: 2000, // 시간 설정
       disableOnInteraction: false, // false-스와이프 후 자동 재생
     },
 
@@ -55,73 +55,24 @@ var swiper = new Swiper(".mySwiper", {
 
   });
 
+  var swiper = new Swiper(".mySwiper2", {
+    spaceBetween: 0,   // 슬라이드 사이 여백
 
-// $("document").ready(function () {
-//   // 문서의 dom이 준비되면
-//   // .mySwiper 클래스를 swiper 슬라이더로 만듦
-//   // 이후에 swiper변수에 할당했기 때문에 스크립트로 제어할 수도 있음.
-//   var swiper = new Swiper(".mySwiper", {
-//       slidesPerView: 'auto',
-//       spaceBetween: 80,
-//       autoplay: {     //자동슬라이드 (false-비활성화)
-//       delay: 2500, // 시간 설정
-//       disableOnInteraction: false, // false-스와이프 후 자동 재생
-//      },
-//       loop: true, //슬라이드 반복 여부
+    slidesPerView : 'auto', // 한 슬라이드에 보여줄 갯수
+    centeredSlides: true,    //센터모드
+    autoplay: {     //자동슬라이드 (false-비활성화)
+      delay: 2000, // 시간 설정
+      disableOnInteraction: false, // false-스와이프 후 자동 재생
+    },
 
+    loop : true,   // 슬라이드 반복 여부
 
-//      loopAdditionalSlides : 1,
+  pagination : { // 페이징
+      el : '.swiper-pagination',
+      clickable : true, // 페이징을 클릭하면 해당 영역으로 이동, 필요시 지정해 줘야 기능 작동
+  }
 
-
-//       // pagination 기본은 bullet
-//       pagination: {
-//           el: ".swiper-pagination"
-//       },
-
-//       // 좌우 화살표 navigation element 지정
-//       navigation: {
-//           nextEl: ".swiper-button-next",
-//           prevEl: ".swiper-button-prev"
-//       },
-//       // 반응형
-//       breakpoints: {
-//           // 600px 이하가 되면 슬라이드 간 간격을 0으로
-//           600: {
-//               spaceBetween: 0
-//           },
-//       },
-
-//       on: {
-//           init: function() {
-//               $('.swiper-slide').addClass('changed');
-
-//               // fraction에 현재 인덱스와 전체 인덱스 표시
-//               // this.loopedSlides는 loop, slidesPerView: 'auto'일 때 제대로 동작
-//               $('.custom-fraction .current').text(this.realIndex + 1);
-//               $('.custom-fraction .all').text(this.loopedSlides);
-//               // console.log(this);
-//               // console.log(this.loopedSlides)
-//           },
-
-//           slideChangeTransitionStart: function() {
-//               // 기본적으로 제공하는 swiper-slide-active 클래스를 이용해
-//               // css transition 애니메이션 작성 가능하다.
-//               // 다만 루프 모드일 때에는 루프 픽스를 하며 slide를 바꿔치기를 하는데,
-//               // 이 때 플리커링이 발생할 수 있다.
-//               // css transition을 서로 다르게 설정한 changed, changing 클래스를 이용
-//               $('.swiper-slide').addClass('changing');
-//               $('.swiper-slide').removeClass('changed');
-
-//               // 페이지 넘어갈 때마다 fraction 현재 인덱스 변경
-//               $('.custom-fraction .current').text(this.realIndex + 1);
-//           },
-
-//       },
-//   });
-
-
-// });
-
+  });
 
 
 
